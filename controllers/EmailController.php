7f276e263914d1a4e5b1524c52a2ebbe9b0b733a
duongntph33240 <duongntph33240@fpt.edu.vn>
2023-12-04 14:17:26 +0700

    commit

--- controllers/EmailController.php
+++ controllers/EmailController.php
@@ -5,30 +5,49 @@ class EmailController{
         if (isset($_GET['nhapEmail'])){
             // Chưa có check email
             $EmailDAO = new EmailDAO();
-            $maCode = time();
-            $_SESSION['maCode'] = $maCode;
+            $TaiKhoanDAO = new TaiKhoanDAO();
             $email = $_POST['email'];
-            $EmailDAO->quenMatKhau($maCode,$email);
-            include_once "views/dangNhap/NhapCode.php";
-        }elseif (isset($_GET['nhapCode'])){
-            if ($_POST['maCode'] == $_SESSION['maCode']){
-                $email = $_POST['email'];
-                include_once "views/dangNhap/MatKhauMoi.php";
-            }else{
-                //Thiếu thông báo nhập sai mã code
-                $email = $_POST['email'];
-                include_once "views/dangNhap/NhapCode.php";
-            }
+            $user = $TaiKhoanDAO->showEmail($email);
+            $id = $user[0]->id;
+            $EmailDAO->quenMatKhau($id,$email);
+            include_once "views/dangNhap/NhapEmail.php";
         }elseif (isset($_GET['datLaiMatKhau'])){
-            unset($_SESSION['maCode']);
-            if ($_POST['mk1'] == $_POST['mk2']){
-                $TaiKhoanDAO = new TaiKhoanDAO();
-                $TaiKhoanDAO->updateMK($_POST['mk1'],$_POST['email']);
-                header("location: index.php?controller=dangNhap");
-            }else{
-                //Thiếu thông báo nhập không trùng mk
-                $email = $_POST['email'];
-                include_once "views/dangNhap/MatKhauMoi.php";
+            if (isset($_GET['id']) && isset($_GET['email'])){
+                if (isset($_POST['mk1']) && isset($_POST['mk2']) && $_POST['mk1'] != $_POST['mk2']){
+                    $TaiKhoanDAO = new TaiKhoanDAO();
+                    $check = $TaiKhoanDAO->checkUS($_POST['email'],$_POST['id']);
+                    if (empty($check)){
+                        // Thiếu thông báo sai tài khoản
+                        echo "không tìm được tài khoản này";
+                    }else{
+                        $id = $_GET['id'];
+                        $email = $_GET['email'];
+                        include_once "views/dangNhap/MatKhauMoi.php";
+                    }
+                }else if (!isset($_POST['mk1']) && !isset($_POST['mk2'])){
+                    $TaiKhoanDAO = new TaiKhoanDAO();
+                    $check = $TaiKhoanDAO->checkUS($_GET['email'],$_GET['id']);
+                    if (empty($check)){
+                        // Thiếu thông báo sai tài khoản
+                        echo "không tìm được tài khoản này";
+                    }else{
+                        $id = $_GET['id'];
+                        $email = $_GET['email'];
+                        include_once "views/dangNhap/MatKhauMoi.php";
+                    }
+                }
+            }
+            if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['mk1']) && isset($_POST['mk2']) && isset($_POST['email']) && isset($_POST['id'])){
+                if ($_POST['mk1'] == $_POST['mk2']){
+                    $TaiKhoanDAO = new TaiKhoanDAO();
+                    $TaiKhoanDAO->updateMK($_POST['mk1'],$_POST['email']);
+                    header("location: index.php?controller=dangNhap");
+                }else{
+                    //Thiếu thông báo nhập không trùng mk
+                    $email = $_POST['email'];
+                    $id = $_POST['id'];
+                    header("location: http://localhost:63342/Nhom01_WebBanHang_Edubook/index.php?controller=quenMatKhau&datLaiMatKhau&id=".$id."&email=".$email);
+                }
             }
         }else{
             include_once "views/dangNhap/NhapEmail.php";
